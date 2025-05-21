3510af501f6e41b2daf66460d85dc2b6cc148ea8
AlexanderLivanov <help.underlinegroup@gmail.com>
2025-05-21 19:32:34 +0300

    bugfix

--- devs/select.php
+++ devs/select.php
@@ -1,29 +1,31 @@
 <?php
-    session_start();
-    require_once('../swad/config.php');
-    require_once('../swad/controllers/organization.php');
-    require_once('../swad/controllers/user.php');
+session_start();
+require_once('../swad/config.php');
+require_once('../swad/controllers/organization.php');
+require_once('../swad/controllers/user.php');
 
-    $curr_user = new User();
-    $db = new Database();
+$curr_user = new User();
+$db = new Database();
 
-    if (empty($_SESSION['logged-in']) or $curr_user->checkAuth() > 0) {
-        echo ("<script>window.location.replace('../login');</script>");
-    }
+if (empty($_SESSION['logged-in']) or $curr_user->checkAuth() > 0) {
+    echo ("<script>window.location.replace('../login');</script>");
+}
 
-    $user_id = $db->Select(
+$user_id = $db->Select(
     "SELECT id FROM `users`
                     WHERE `telegram_id` = :id",
     [
         'id' => $_SESSION['telegram_id']
-    ]);
+    ]
+);
 
-    $user_orgs = $db->Select(
+$user_orgs = $db->Select(
     "SELECT name FROM `organizations`
                         WHERE `owner_id` = :id",
     [
         'id' => $user_id[0][0]
-    ]);
+    ]
+);
 ?>
 
 <!DOCTYPE html>
@@ -146,20 +148,21 @@
     <div class="console-container">
         <h1 class="title">Выберите студию</h1>
 
-        <?php foreach($user_orgs as $org): ?>
-        <ul class="studio-list">
-            <li class="studio-item">
-                <i class="material-icons studio-icon">business</i>
-                <span class="studio-name"><?= $org[0] ?></span>
-            </li>
-        </ul>
+        <?php foreach ($user_orgs as $org): ?>
+            <ul class="studio-list">
+                <li class="studio-item">
+                    <i class="material-icons studio-icon">business</i>
+                    
+                    <span class="studio-name" onclick="location.href='devs'"><?= $org[0] ?></span>
+                </li>
+            </ul>
         <?php endforeach; ?>
 
-        <?php if(count($user_orgs) < 1): ?>
-        <div class="empty-state">
-            <i class="material-icons" style="font-size: 48px; margin-bottom: 16px;">business</i>
-            <p>У вас пока нет ни одной студии</p>
-        </div>
+        <?php if (count($user_orgs) < 1): ?>
+            <div class="empty-state">
+                <i class="material-icons" style="font-size: 48px; margin-bottom: 16px;">business</i>
+                <p>У вас пока нет ни одной студии</p>
+            </div>
         <?php endif; ?>
 
 
